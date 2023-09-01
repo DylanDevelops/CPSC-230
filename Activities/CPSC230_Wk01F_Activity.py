abc3e7fc7c3e376b11766ead6e005e516b3ab011
Dylan Ravel <dylanm.ravel@gmail.com>
2023-09-01 11:31:52 -0700

    added comments

--- Activities/CPSC230_Wk01F_Activity.py
+++ Activities/CPSC230_Wk01F_Activity.py
@@ -6,6 +6,8 @@
 Then on the next line, print out number_of_units + 1. 
 What does it print out?
 '''
+
+# set a variable 'number_of_units' to 3 and then printed that value plus 1 in the console
 number_of_units = 3
 print("Number of units:", number_of_units + 1)
 
@@ -13,14 +15,16 @@ print("Number of units:", number_of_units + 1)
 2. What's wrong with the following lines of code? Can you fix the error
 and print the result?
 '''
+
+# originally had a 10 as the variable name which is a literal and it isn't printing anything
 x = 5
 print("x + x is:", x + x)
-
 
 '''
 3. What's wrong with the following line of code? Can you fix it?
 '''
 
+# You can't have a number (literal) as a variable name; originally has 2 as the first character
 tooCoolForSchool = "John"
 print("Hello", tooCoolForSchool + "!")
 
@@ -28,5 +32,6 @@ print("Hello", tooCoolForSchool + "!")
 4. What's wrong with the following lines of code? Can you fix it?
 '''
 
+# eel originally had a capital E in the print statement
 eel = "Long Fish"
 print("The American eel is a " + eel)
