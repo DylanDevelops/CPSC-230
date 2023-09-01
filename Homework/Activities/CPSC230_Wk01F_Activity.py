f11ef21e6fbda9e3ca759e83ef40de328b836c2f
Dylan Ravel <dylanm.ravel@gmail.com>
2023-09-01 11:14:24 -0700

    Create CPSC230_Wk01F_Activity.py

---
+++ Homework/Activities/CPSC230_Wk01F_Activity.py
@@ -0,0 +1,32 @@
+# Class Activities + Review (Week01 Friday)
+
+# Classwork-----------------------------------------------
+'''
+1. Create a variable called number_of_units and set it equal to 3. 
+Then on the next line, print out number_of_units + 1. 
+What does it print out?
+'''
+number_of_units = 3
+print("Number of units:", number_of_units + 1)
+
+'''
+2. What's wrong with the following lines of code? Can you fix the error
+and print the result?
+'''
+x = 5
+print("x + x is:", x + x)
+
+
+'''
+3. What's wrong with the following line of code? Can you fix it?
+'''
+
+tooCoolForSchool = "John"
+print("Hello", tooCoolForSchool + "!")
+
+'''
+4. What's wrong with the following lines of code? Can you fix it?
+'''
+
+eel = "Long Fish"
+print("The American eel is a " + eel)
