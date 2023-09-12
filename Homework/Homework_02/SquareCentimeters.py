54ed8ab32ab6204b9e123d429f1c097f6568adf4
Dylan Ravel <dylanm.ravel@gmail.com>
2023-09-11 18:08:06 -0700

    created file for third document but haven't finished it yet

---
+++ Homework/Homework_02/SquareCentimeters.py
@@ -0,0 +1,20 @@
+# --------------------------------------------
+# Dylan Ravel
+# September 11, 2023
+# Celsius Assignment
+# References
+# 1. 
+#
+# README
+# 
+# -----------------------------------------------
+
+'''
+Create a program called “SquareCentimeters.py”
+Say the user has a pet reptile. When they are going to buy a tank, they find that the size is typically given in liters. The user wants to calculate the footprint of a tank in square centimeters so that they are sure their pet will have enough space to comfortably move around. Assume the tank is a perfect cube (length, width, and height are the same) and write a program that calculates the area of the footprint of the tank in square centimeters.
+1) Prompt the user for the volume of the tank in liters
+2) Prompt the user for the species of reptile they plan to adopt.
+3) Calculate the footprint of the tank in square centimeters (hint, you can use a function in the math module to calculate the cube root)
+4) Display the area to the user
+'''
+
