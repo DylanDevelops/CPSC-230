fada02509f48c22a2290b21be54a4d26d25bd934
Dylan Ravel <dylanm.ravel@gmail.com>
2023-10-17 01:21:52 -0700

    for the movie

---
+++ ScratchWork.py
@@ -0,0 +1,21 @@
+caroline = 0
+nick = 0
+katelyn = 0
+
+while True:
+    userInput = input("C, N, K or DONE to finish: ").lower()
+    if userInput == "c":
+        caroline += 1
+        print("Added to caroline")
+    elif userInput == "n":
+        nick += 1
+        print("Added to nick")
+    elif userInput == "k":
+        katelyn += 1
+        print("Added to katelyn")
+    elif userInput == "done":
+        break
+    else:
+        print("Not a valid choice!")
+
+print(f"Final Scores:\nCaroline: {caroline}\nNick: {nick}\nKatelyn: {katelyn}")
