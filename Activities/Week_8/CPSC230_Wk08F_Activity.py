e2accad226c48a75c959158700041f65ec2d2d15
Dylan Ravel <dylanm.ravel@gmail.com>
2023-10-20 12:57:46 -0700

    Added a distance function that calculates the distance between two points in 2D space. The function now has default arguments for the first point, allowing the user to calculate the distance from the origin if desired. This function will be useful for various calculations in the future.

---
+++ Activities/Week_8/CPSC230_Wk08F_Activity.py
@@ -0,0 +1,52 @@
+# Class Activities
+
+### If you weren't able to finish this on Wednesday, work on it today
+### NOTE: Add a docstring to your distance function
+'''
+0.  Distance Formula
+    NOTE: Add default arguments for pt1 (pt1 = [0,0])
+    Write a functions that calculates the distance between two points
+    https://en.wikipedia.org/wiki/Distance (just 2D distance is fine)
+        - The function should take 2 lists as inputs
+            - pt1 = [x1, y1]
+            - pt2 = [x2, y2]
+        - The function should return the distance
+        - Remember that a distance cannot be a negative number
+        - BONUS 01: Write your script so that the user can enter the 
+          X and Y coordinates of their two points
+        - BONUS 02: Write your function so it only returns 3 
+          significant digits
+    
+    NOTE: Add to your function to give the user the option to cauclulate 
+          either Euclidean or Manhattan distance (Euclidean) is the formula 
+          you used Monday, Manhattan is linked here:
+          https://algodaily.com/lessons/what-is-the-manhattan-distance
+'''
+## The answer with these coordinates will be 13.928 (Euclidean)
+pt1 = [5, 2]
+pt2 = [10, 15]
+
+## Worked on my game but I will revisit this later to make sure it makes sense.
+
+
+
+
+'''
+1.  Team up with 1-2 folks at your table. Share your progress with the 
+    project (you can use your table monitors if that's easier). 
+    
+    If all of you are finished with your code, share and play each 
+    others games and give feedback. Consider:
+        1) How could your classmates make their game more interactive?
+        2) How could they make it more user-friendly?
+        3) Are there enough comments in their code?
+    
+    If some of you are still working, help your classmates brainstorm 
+    and debug their code.
+
+    Nothing to turn in for this one.
+'''
+
+
+
+
